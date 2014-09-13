c3424f2623d57d248aed83c551bd9bbbac892a53
rssilva <rafaelspdasilva@gmail.com>
2014-09-13 19:03:28 -0300

    initializing and appending mapview

--- assets/js/views/main.view.js
+++ assets/js/views/main.view.js
@@ -10,8 +10,18 @@ window.App.Views.MainView = Backbone.View.extend({
 		this.$el.append(view.render().$el);
 	},
 
+	appendMap: function () {
+		var model = new App.Models.MapModel({
+			center: {lat: -32.51, lng: -52.52},
+			zoom: 10
+		});
+		var view = new App.Views.MapView({model: model});
+		this.$el.append(view.render().$el);
+	},
+
 	render: function () {
 		this.appendAbout();
+		this.appendMap();
 		return this;
 	}
 });
