75f4e35e5e0931a4a90bd50967f9ecf92c7b734d
Raphael <raphasilvac@gmail.com>
2014-09-13 22:08:04 -0300

    Added api accidents

---
+++ api/controllers/AccidentsController.js
@@ -0,0 +1,11 @@
+/**
+ * AccidentsController
+ *
+ * @description :: Server-side logic for managing accidents
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
